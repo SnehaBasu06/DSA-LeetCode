3054288b8c103f30fe7a65d525ea3f7e6139582e
Sneha Basu <94731983+SnehaBasu06@users.noreply.github.com>
2024-06-02 19:40:35 +0530

    Update 125. Valid Palindrome.cpp

--- Char Arrays & Strings/125. Valid Palindrome.cpp
+++ Char Arrays & Strings/125. Valid Palindrome.cpp
@@ -1,1 +1,41 @@
-125. Valid Palindrome
+class Solution {
+public:
+    bool validchar(char ch){
+        if(ch>='a' && ch<='z' || ch>='A' && ch<='Z' ||ch>='0' && ch<='9'){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    char toLower(char ch){
+        if(ch>='a' && ch<='z'){
+            return ch;
+        }
+        else{
+            return ch-'A'+'a';
+        }
+    }
+    bool isPalindrome(string s) {
+        int n=s.size();
+        int start =0;
+        int end = n-1;
+        while(start<=end){
+            if(!validchar(s[start])){
+                start++;
+            }
+            else if(!validchar(s[end])){
+                end--;
+            }
+            else if(toLower(s[start])==toLower(s[end])){
+                start++;
+                end--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
